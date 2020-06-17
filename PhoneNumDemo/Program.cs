5c714be53d88e129db90297a1f616619e7b53418
Sam Nasr <sam@nasr.info>
2020-06-17 17:09:57 -0400

    Add files via upload

---
+++ PhoneNumDemo/Program.cs
@@ -0,0 +1,117 @@
+﻿using Microsoft.Azure.CognitiveServices.Vision.ComputerVision;         //Requires NuGet package "Microsoft.Azure.CognitiveServices.Vision.ComputerVision"
+using Microsoft.Azure.CognitiveServices.Vision.ComputerVision.Models;  //Requires NuGet package "Microsoft.Azure.CognitiveServices.Vision.ComputerVision"
+using System;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Twilio;                   //Requires NuGet Package "Twilio"
+using Twilio.Rest.Lookups.V1;   //Requires NuGet Package "Twilio"
+using Microsoft.Extensions.Configuration;  //Requires NuGet Package "Microsoft.Extensions.Configuration"
+
+namespace PhoneNumDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var config = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+
+            //Replace the following fields with your own specific subscription Key.  Otherwise, the service call will fail.
+            string subscriptionKey = config["AppSettings:subscriptionKey"];
+            string endpoint = config["AppSettings:endpoint"];
+
+            ComputerVisionClient client = InstantiateClient(endpoint, subscriptionKey);
+            string fileName = @"D:\PhoneNumDemo\PhoneNumDemo\Images\Card.jpg";
+            GetTextFromImage(client, fileName).Wait();
+        }
+
+
+        public static ComputerVisionClient InstantiateClient(string endpoint, string key)
+        {
+            ComputerVisionClient client = new ComputerVisionClient(new ApiKeyServiceClientCredentials(key)) { Endpoint = endpoint };
+            return client;
+        }
+
+
+        public static async Task GetTextFromImage(ComputerVisionClient client, string imageFileName)
+        {
+            using (Stream stream = File.OpenRead(imageFileName))
+            {
+                // Get the recognized text
+                OcrResult localFileOcrResult = await client.RecognizePrintedTextInStreamAsync(true, stream);
+
+                // Display text, language, angle, orientation, and regions of text from the results.
+                Console.WriteLine("File Name: " + imageFileName);
+                Console.WriteLine("Language: " + localFileOcrResult.Language);
+                Console.WriteLine("Text Angle: " + localFileOcrResult.TextAngle);
+                Console.WriteLine("Orientation: " + localFileOcrResult.Orientation);
+                Console.WriteLine();
+                Console.WriteLine("Text regions: ");
+
+                // Getting only one line of text for testing purposes. To see a full demonstration, remove the counter & conditional.
+                int counter = 0;
+                foreach (var localRegion in localFileOcrResult.Regions)
+                {
+                    Console.WriteLine("Region bounding box: " + localRegion.BoundingBox);
+                    foreach (var line in localRegion.Lines)
+                    {
+                        Console.WriteLine("Line bounding box: " + line.BoundingBox);
+                        counter++;
+
+                        foreach (var word in line.Words)
+                        {
+                            Console.WriteLine("Word bounding box: " + word.BoundingBox);
+                            Console.WriteLine("Text: " + word.Text);
+
+                            //Vaidate using RegEx
+                            Regex rx = new Regex(@"^[2-9]\d{2}-\d{3}-\d{4}$", RegexOptions.IgnorePatternWhitespace);
+                            MatchCollection matches = rx.Matches(word.Text);
+
+                            if (matches.Count >= 1)
+                            {
+                                Console.WriteLine("Phone Number Found: " + matches.First().ToString());
+                                IsTwilioVerified(matches.First().ToString());
+                            }
+                        }
+
+                        Console.WriteLine();  //Skip a space
+                    }
+                }
+            }
+        }
+
+
+        private static bool IsTwilioVerified(string num)
+        {
+            bool IsValid = false;
+            var config = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+
+            // Find your Account Sid and Token at twilio.com/console. See http://twil.io/secure for more info
+            string accountSid = config["AppSettings:accountSid"];
+            string authToken = config["AppSettings:authToken"];
+
+            try
+            {
+                TwilioClient.Init(accountSid, authToken);
+
+                //Reference: https://www.twilio.com/docs/lookup/tutorials/validation-and-formatting
+                var numInfo = PhoneNumberResource.Fetch(countryCode: "US", pathPhoneNumber: new Twilio.Types.PhoneNumber(num));
+                Console.WriteLine("Twilio Verified Phone Number: " + numInfo.PhoneNumber);
+
+                if (numInfo.PhoneNumber.ToString().Length >= 1)
+                    IsValid = true;
+            }
+
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error:" + ex.Message);
+                IsValid = false;
+            }
+
+            return IsValid;
+        }
+
+
+    }
+}      
